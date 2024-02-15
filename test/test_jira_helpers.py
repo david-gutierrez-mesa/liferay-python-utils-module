178630cac9082697b2138be6bd0e503689148f62
david-gutierrez-mesa <david.gutierrez@liferay.com>
2024-02-15 18:03:19 +0100

    LPU-0004 Test for new method

---
+++ test/test_jira_helpers.py
@@ -0,0 +1,28 @@
+import unittest
+
+from liferay_utils.jira_utils.jira_helpers import initialize_subtask_patch_release
+from liferay_utils.jira_utils.jira_liferay import get_jira_connection
+
+
+class EchoTestMapTests(unittest.TestCase):
+
+    def test_initialize_subtask_patch_release(self):
+        try:
+            expected = {'project': {'key': 'LPD'}, 'summary': 'LPD-15582',
+                        'description': 'https://liferay.atlassian.net/browse/LPD-15582',
+                        'issuetype': {'name': 'Technical Task'},
+                        'parent': {'id': '1038479'},
+                        'components': [{'name': 'Release'}]}
+            jira = get_jira_connection()
+            parent_lps = jira.issue('LPD-16987', fields='id')
+            summary = 'LPD-15582'
+            sub_task = initialize_subtask_patch_release(parent_lps, summary)
+            self.assertEqual(sub_task, expected)
+            print(sub_task)
+
+        except Exception:
+            self.fail("Test failed unexpectedly!")
+
+
+if __name__ == '__main__':
+    unittest.main()
